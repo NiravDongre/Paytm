0ea92d73aa9fdf7e4d0936957a06605c92a0ad14
Nirav Dongre <nsd3284@gmail.com>
2026-04-16 15:33:33 +0530

    Adding Pagination in transactions and in users

--- backend/controllers/transaction.controller.js
+++ backend/controllers/transaction.controller.js
@@ -13,7 +13,7 @@ const history = asyncHandler(async(req, res, next) => {
         return next(new CustomError("Account not found", 404))
     }
 
-    const transaction = {
+    const filter = {
         $or:[
             { fromAccount: account._id },
             { toAccount: account._id}
@@ -24,20 +24,21 @@ const history = asyncHandler(async(req, res, next) => {
     const limit = req.query.limit*1 || 10;
     const skip = ( page - 1) * limit
 
-    const transactioncount = await TransactionModel.countDocuments(transaction);
+    const total = await TransactionModel.countDocuments(filter);
 
-    if(skip >= transactioncount){
+    if(skip >= total){
         return next(new CustomError("Page not Found", 404))
     }
 
     const transactions = await TransactionModel
     .find(filter)
+    .sort("-createdAt")
     .skip(skip)
     .limit(limit)
 
     return res.status(200).json({
         results: transactions.length,
-        transactioncount,
+        total,
         data: transactions
    })
 })
